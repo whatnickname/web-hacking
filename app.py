3bd3bcf11e18135f05d6f685b3d9d2134bc8bc43
whatnickname <qlqjs1332@gmail.com>
2025-05-19 13:26:39 +0900

    commit message

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from website import create_app
 from flask import Flask, render_template, request, redirect, url_for
 from website import database  #경로 수정
-from website import db, create_app
+from website import db, create_app #.
 
 app = create_app()
 
